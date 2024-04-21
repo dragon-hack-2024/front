b0ef0b5e2c029df9b01105b5c8c32e49c9dce77e
MrHorvatHD <mh3418@student.uni-lj.si>
2024-04-21 09:37:16 +0200

    Dela v1

--- src/app/(routes)/workout/page.tsx
+++ src/app/(routes)/workout/page.tsx
@@ -19,6 +19,8 @@ import {
   RotateCwIcon,
 } from "lucide-react";
 import Scan from "@/components/Scan";
+import SaveDialog from "@/components/saveDialog";
+import { log } from "console";
 
 interface User {
   id: number;
@@ -27,6 +29,15 @@ interface User {
   weight: number;
   height: number;
   birth_date: string;
+}
+
+interface Data {
+  duration: number;
+  score: number;
+  calories_burned: string;
+  rpm: number;
+  challenge_id: number;
+  user_id: number;
 }
 
 function WorkoutComponent() {
@@ -52,7 +63,6 @@ function WorkoutComponent() {
     if (active.current) {
       timer = setInterval(() => {
         setElapsedTime((prev) => prev + 0.1);
-        setCalories((prev) => prev + (0.1 * currentRPM) / 600); // Example calorie calculation
         if (isRpmsMatch()) {
           setMatchTime((prev) => prev + 0.1);
         }
@@ -99,6 +109,8 @@ function WorkoutComponent() {
       lastPingTime.current = currentTime; // Update last ping time
 
       if (active.current) {
+        const weight = user.weight || 70; // Default weight is 70 kg
+        setCalories((prev) => prev + (12 * weight * 3.5) / 200 / 120);
         setRpmHistory((prevHistory) => {
           const newHistory = [...prevHistory, rpm].slice(-5); // Keep only the last 5 entries
           setCurrentRPM(
@@ -139,8 +151,15 @@ function WorkoutComponent() {
     return () => clearTimeout(timeoutRef.current);
   }, []);
 
+  const [openSaveDialog, setOpenSaveDialog] = useState(false);
+  const [workoutData, setWorkoutData] = useState<Data>({} as Data);
+
+  const handleCloseDialog = () => {
+    setOpenSaveDialog(false);
+  };
+
   const finishWorkout = async () => {
-    const data = {
+    const data: Data = {
       calories_burned: calories.toFixed(0),
       rpm: jumpCount.current / (elapsedTime / 60),
       duration: elapsedTime,
@@ -150,177 +169,189 @@ function WorkoutComponent() {
     };
 
     console.log(data);
+    setWorkoutData(data);
 
-    // Send data to server
-    /*const response = await fetch("http://localhost:8080/v1/stats", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    if (elapsedTime > 3) {
+      setOpenSaveDialog(true);
 
-    if (response.ok) {
-      console.log("Data sent successfully");
-    } else {
-      console.error("Failed to send data to server");
-    }*/
+      // Send data to server
+      const response = await fetch("http://localhost:8080/v1/stats", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log("Data sent successfully");
+      } else {
+        console.error("Failed to send data to server");
+      }
+    }
   };
 
   return (
-    <Paper
-      sx={{
-        paddingY: 2,
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Scan onDataReceived={handleDataReceived} />
-
-      <Box display="flex" alignItems="center" justifyContent="space-between">
-        <Box
-          width={"35vw"}
-          sx={{
-            backgroundColor: theme.palette.grey[100],
-            borderRadius: "7px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            mr: 2,
-          }}
-        >
-          <RotateCwIcon size={30} />
-          <Typography
-            variant="h3"
-            ml="2"
-            sx={{
-              my: 2,
-              ml: 1,
-            }}
-          >
-            {currentRPM.toFixed(0).toString().padStart(3, "0")}
-          </Typography>
-        </Box>
-
-        <Box
-          width={"35vw"}
-          sx={{
-            backgroundColor: theme.palette.grey[100],
-            borderRadius: "7px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            ml: 2,
-          }}
-        >
-          <FlameIcon size={30} />
-          <Typography
-            variant="h3"
-            sx={{
-              my: 2,
-              ml: 1,
-            }}
-          >
-            {calories.toFixed(2)}
-          </Typography>
-        </Box>
-      </Box>
-      <Box
+    <>
+      <SaveDialog
+        open={openSaveDialog}
+        onClose={handleCloseDialog}
+        data={workoutData}
+      />
+      <Paper
         sx={{
-          height: "55vh",
-          width: "100%",
+          paddingY: 2,
+          width: "100vw",
+          height: "100vh",
           display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
           justifyContent: "center",
-          position: "relative",
-          m: 2,
-          overflow: "hidden",
         }}
       >
+        <Scan onDataReceived={handleDataReceived} />
+
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <Box
+            width={"35vw"}
+            sx={{
+              backgroundColor: theme.palette.grey[100],
+              borderRadius: "7px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              mr: 2,
+            }}
+          >
+            <RotateCwIcon size={30} />
+            <Typography
+              variant="h3"
+              ml="2"
+              sx={{
+                my: 2,
+                ml: 1,
+              }}
+            >
+              {currentRPM.toFixed(0).toString().padStart(3, "0")}
+            </Typography>
+          </Box>
+
+          <Box
+            width={"35vw"}
+            sx={{
+              backgroundColor: theme.palette.grey[100],
+              borderRadius: "7px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              ml: 2,
+            }}
+          >
+            <FlameIcon size={30} />
+            <Typography
+              variant="h3"
+              sx={{
+                my: 2,
+                ml: 1,
+              }}
+            >
+              {calories.toFixed(0)}
+            </Typography>
+          </Box>
+        </Box>
         <Box
           sx={{
-            position: "absolute",
-            top: 0,
-            width: "50%",
             height: "55vh",
-            borderRadius: "7px",
-            backgroundColor: theme.palette.grey[100],
-          }}
-        ></Box>
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: 0,
-            width: "50%",
-            height: (currentRPM / maxRPM) * 55 + "vh",
-            borderRadius: "7px",
-            backgroundColor: isRpmsMatch()
-              ? theme.palette.primary.main
-              : theme.palette.error.main,
-            opacity: 0.8,
-            zIndex: 5,
-            transition: "height 0.5s ease",
-          }}
-        ></Box>
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: (targetRPM / maxRPM) * 55 - 5 + "vh",
-            width: "50%",
-            height: "10vh",
-            borderRadius: "7px",
-            opacity: 0.5,
-            zIndex: 10,
-            backgroundColor: theme.palette.grey[400],
+            width: "100%",
             display: "flex",
             justifyContent: "center",
-            alignItems: "center",
+            position: "relative",
+            m: 2,
+            overflow: "hidden",
           }}
         >
-          <Typography variant="h3" sx={{ color: "black" }}>
-            {targetRPM}
-          </Typography>
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              width: "50%",
+              height: "55vh",
+              borderRadius: "7px",
+              backgroundColor: theme.palette.grey[100],
+            }}
+          ></Box>
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: 0,
+              width: "50%",
+              height: (currentRPM / maxRPM) * 55 + "vh",
+              borderRadius: "7px",
+              backgroundColor: isRpmsMatch()
+                ? theme.palette.primary.main
+                : theme.palette.error.main,
+              opacity: 0.8,
+              zIndex: 5,
+              transition: "height 0.5s ease",
+            }}
+          ></Box>
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: (targetRPM / maxRPM) * 55 - 5 + "vh",
+              width: "50%",
+              height: "10vh",
+              borderRadius: "7px",
+              opacity: 0.5,
+              zIndex: 10,
+              backgroundColor: theme.palette.grey[400],
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h3" sx={{ color: "black" }}>
+              {targetRPM}
+            </Typography>
+          </Box>
         </Box>
-      </Box>
 
-      <Box sx={{ display: "flex" }}>
-        <IconButton
-          onClick={handleStartPauseToggle}
-          sx={{
-            mx: 1,
-            background: theme.palette.primary.main,
-            "&:hover": { background: theme.palette.primary.main },
-          }}
-        >
-          {active.current ? (
-            <Pause size={24} color={"white"} />
-          ) : (
-            <Play size={24} color={"white"} />
-          )}
-        </IconButton>
+        <Box sx={{ display: "flex" }}>
+          <IconButton
+            onClick={handleStartPauseToggle}
+            sx={{
+              mx: 1,
+              background: theme.palette.primary.main,
+              "&:hover": { background: theme.palette.primary.main },
+            }}
+          >
+            {active.current ? (
+              <Pause size={24} color={"white"} />
+            ) : (
+              <Play size={24} color={"white"} />
+            )}
+          </IconButton>
 
-        <Typography
-          variant="h2"
-          sx={{ mb: 1, width: "20vw", textAlign: "center" }}
-        >
-          {formatTime(elapsedTime.toFixed(0))}
-        </Typography>
+          <Typography
+            variant="h2"
+            sx={{ mb: 1, width: "20vw", textAlign: "center" }}
+          >
+            {formatTime(elapsedTime.toFixed(0))}
+          </Typography>
 
-        <IconButton
-          onClick={handleStop}
-          sx={{
-            mx: 1,
-            backgroundColor: theme.palette.primary.main,
-            "&:hover": { background: theme.palette.primary.main },
-          }}
-        >
-          <Square size={24} color={"white"} />
-        </IconButton>
-      </Box>
-    </Paper>
+          <IconButton
+            onClick={handleStop}
+            sx={{
+              mx: 1,
+              backgroundColor: theme.palette.primary.main,
+              "&:hover": { background: theme.palette.primary.main },
+            }}
+          >
+            <Square size={24} color={"white"} />
+          </IconButton>
+        </Box>
+      </Paper>
+    </>
   );
 }
 
---
+++ src/components/saveDialog.tsx
@@ -0,0 +1,96 @@
+"use client";
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/material";
+import {
+  CalendarIcon,
+  FlameIcon,
+  RotateCwIcon,
+  TargetIcon,
+  TimerIcon,
+} from "lucide-react";
+
+interface Data {
+  duration: number;
+  score: number;
+  calories_burned: string;
+  rpm: number;
+  challenge_id: number;
+  user_id: number;
+}
+
+function SaveDialog({
+  open,
+  onClose,
+  data,
+}: {
+  open: boolean;
+  onClose: () => void;
+  data: Data;
+}) {
+  const formatTime = (timeInSeconds: number) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    const secondsStr = seconds.toFixed(0);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${secondsStr}`;
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Workout Summary</DialogTitle>
+      <DialogContent sx={{ width: "70vw" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "column",
+            mt: 2,
+            borderRadius: "7px",
+            paddingX: "1rem",
+            paddingY: "0.75rem",
+            "--tw-shadow":
+              "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+            "--tw-shadow-colored":
+              "0 1px 3px 0 var(--tw-shadow-color), 0 1px 2px -1px var(--tw-shadow-color)",
+            boxShadow:
+              "var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)",
+          }}
+        >
+          <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+            <Box sx={{ display: "flex", justifyItems: "center" }}>
+              <TimerIcon size={16} style={{ marginRight: "0.2rem" }} />
+              <Typography>{formatTime(data?.duration || 0)}</Typography>
+            </Box>
+
+            <Box sx={{ display: "flex", justifyItems: "center" }}>
+              <TargetIcon size={16} style={{ marginRight: "0.2rem" }} />
+              <Typography>{data?.score}%</Typography>
+            </Box>
+          </Box>
+
+          <Box sx={{ display: "flex", justifyContent: "space-around", mt: 2 }}>
+            <Box sx={{ display: "flex", justifyItems: "center" }}>
+              <FlameIcon size={16} style={{ marginRight: "0.2rem" }} />
+              <Typography>{data?.calories_burned} kcal</Typography>
+            </Box>
+
+            <Box sx={{ display: "flex", justifyItems: "center" }}>
+              <RotateCwIcon size={16} style={{ marginRight: "0.2rem" }} />
+              <Typography>{data?.rpm?.toFixed(2)} RPM</Typography>
+            </Box>
+          </Box>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default SaveDialog;
